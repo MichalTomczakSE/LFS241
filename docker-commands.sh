c51b455acf0bab662d2a082de4f82765a7f6d713
Michal Tomczak <michaltomczakse@gmail.com>
2025-09-04 21:08:06 +0200

    fix&refactor: created separate function for running containers

--- docker-commands.sh
+++ docker-commands.sh
@@ -18,11 +18,23 @@ NODE_EXPORTER_VERSION="v1.9.1"
 NODE_EXPORTER_VOLUME="/:/host:ro,rslave"
 NODE_EXPORTER_IMAGE="docker.io/prom/node-exporter"
 
-show_container_id() { 
-	echo "$1 is already running with id $(docker ps -aq -f name=$1)"
+run_container() {
+	local name=$1
+	local network=$2
+	local image_tag=$3
+	local extra_args=$4
+	local image_args=$5
+
+	if docker ps -a --format '{{.Names}}' | grep -q "${name}" ; then 
+		echo "Container $name is already running with id $(docker ps -aq -f name=$1)"
+	else 
+		echo "Creating container: ${name}"
+		docker run -d --name "${name}" -h "${name}" --net "${network}" ${extra_args} "${image_tag}" ${image_args}
+		echo "Container ${name} created and running successfully"
+	fi
 }
 
-if [ ! docker network inspect $LAB_NETWORK > /dev/null ]; then 
+if ! docker network inspect $LAB_NETWORK > /dev/null; then 
 	echo "Creating network $LAB_NETWORK"
 	docker network create --driver bridge $LAB_NETWORK
 fi
@@ -31,35 +43,16 @@ if [ ! -d LFS241 ]; then
 	git clone --depth=1 https://github.com/lftraining/LFS241.git
 fi
 
-docker images | grep -q prometheus-demo-service && {
-	echo "Image prometehus-demo-service built. Containers are running"
-} || { 
+if  ! docker images --format '{{.Repository}}' | grep -q "prometheus-demo-service"; then 
 	echo "Building prometheus-demo-service" 
 	docker build -t prometheus-demo-service LFS241/demo-service-source/.
-
+else 
+	echo "Docker image exists, running demo service containers" 
 	for i in {1..3}; do
-		echo "Creating d${i} prometheus-demo-service container"
-		docker run -d --name d${i} -h d${i} --net ${LAB_NETWORK} prometheus-demo-service
+		run_container d${i} $LAB_NETWORK prometheus-demo-service 
 	done;
-}
+fi
 
-docker ps -a -f name=$PROM_NAME | grep -q $PROM_NAME && {
-	show_container_id $PROM_NAME
-} || {
-	echo "Creating $PROM_NAME container" 
-	docker run -d -p $PROM_PORT --name $PROM_NAME -h $PROM_NAME --net $LAB_NETWORK -v $PROM_VOLUME  $PROM_IMAGE:$PROM_VERSION
-}
-
-docker ps -a -f name=$GRAFANA_NAME | grep -q $GRAFANA_NAME && { 
-	show_container_id $GRAFANA_NAME
-} || {
-	echo "Creating $GRAFANA_NAME container"
-	docker run -d -p $GRAFANA_PORT --name $GRAFANA_NAME -h $GRAFANA_NAME --net $LAB_NETWORK $GRAFANA_IMAGE:$GRAFANA_VERSION
-}
-
-docker ps -a -f name=$NODE_EXPORTER_NAME | grep -q $NODE_EXPORTER_NAME && { 
-	show_container_id $NODE_EXPORTER_NAME
-} || {
-	echo "Creating $NODE_EXPORTER_NAME container" 
-	docker run -d --name $NODE_EXPORTER_NAME --net $HOST_NETWORK --pid $HOST_NETWORK -v $NODE_EXPORTER_VOLUME $NODE_EXPORTER_IMAGE:$NODE_EXPORTER_VERSION --path.rootfs=/host
-}
+run_container "${PROM_NAME}" "${LAB_NETWORK}" "${PROM_IMAGE}:${PROM_VERSION}" "-v ${PROM_VOLUME} -p ${PROM_PORT}"
+run_container "${GRAFANA_NAME}" "${LAB_NETWORK}" "${GRAFANA_IMAGE}:${GRAFANA_VERSION}" "-p ${GRAFANA_PORT}" 
+run_container "${NODE_EXPORTER_NAME}" "${HOST_NETWORK}" "${NODE_EXPORTER_IMAGE}:${NODE_EXPORTER_VERSION}" "--pid ${HOST_NETWORK} -v ${NODE_EXPORTER_VOLUME}" --path.rootfs=/host
